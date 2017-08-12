185c234e206f82db0ed6ae2abed5f61336f14c7c
Orlando Charles <orlandocharles@hotmail.com>
2017-08-12 15:11:17 -0500

    Add phpunit

---
+++ bootstrap/autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of the CFDI project.
+ *
+ * (c) Orlando Charles <me@orlandocharles.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__.'/../vendor/autoload.php';
