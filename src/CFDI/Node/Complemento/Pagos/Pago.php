89e701b9bb186c3b39d8a0e561131b69758d0caa
Cesar Aguilera <cesargnu29@gmail.com>
2017-10-03 09:03:32 -0500

    Add payment complement nodes

---
+++ src/CFDI/Node/Complemento/Pagos/Pago.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the CFDI project.
+ *
+ * (c) Orlando Charles <me@orlandocharles.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Charles\CFDI\Node\Complemento\Pagos;
+
+/**
+ * This is the timbre fiscal class.
+ *
+ * @author Orlando Charles <me@orlandocharles.com>
+ */
+class Pago extends Pagos
+{
+    /**
+     * @var string
+     */
+    protected $parentNodeName = 'pago10:Pagos';
+    /**
+     * Node name.
+     *
+     * @var string
+     */
+    protected $nodeName = 'pago10:Pago';
+}
