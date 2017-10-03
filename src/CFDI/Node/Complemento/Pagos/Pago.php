d54184648610c8fb3898e6911ac6c8f4d5711896
Cesar Aguilera <cesargnu29@gmail.com>
2017-10-03 10:44:14 -0500

    Add parent attributes in Pago node

--- src/CFDI/Node/Complemento/Pagos/Pago.php
+++ src/CFDI/Node/Complemento/Pagos/Pago.php
@@ -11,13 +11,20 @@
 
 namespace Charles\CFDI\Node\Complemento\Pagos;
 
+use Charles\CFDI\Common\Node;
+
 /**
  * This is the timbre fiscal class.
  *
  * @author Orlando Charles <me@orlandocharles.com>
  */
-class Pago extends Pagos
+class Pago extends Node
 {
+    /**
+     * @var string
+     */
+    protected $wrapperNodeName = 'cfdi:Complemento';
+
     /**
      * @var string
      */
@@ -28,4 +35,26 @@ class Pago extends Pagos
      * @var string
      */
     protected $nodeName = 'pago10:Pago';
+
+    /**
+     * Pago constructor.
+     * @param array $data
+     */
+    public function __construct(array $data = [])
+    {
+        parent::__construct($data, $this->parentAttributes());
+    }
+
+    /**
+     * Node attributes.
+     *
+     * @return array
+     */
+    public function parentAttributes()
+    {
+        return [
+            'xmlns:pago10' => 'http://www.sat.gob.mx/Pagos',
+            'Version' => '1.0',
+        ];
+    }
 }
